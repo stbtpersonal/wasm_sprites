ab5c096857bb569ad2088644f62e03b3265406c3
stbtpersonal <stbt.personal@gmail.com>
2019-11-23 22:20:09 +0200

    Began implementing a simple game loop.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
 
 use canvas::Canvas;
@@ -12,16 +16,48 @@ mod sprite;
 mod point;
 mod logging;
 
+struct State {
+    canvas: Canvas,
+    pikachus: Vec<Sprite<'static>>,
+}
+
 #[wasm_bindgen(start)]
 pub fn main() {
+    let state = initialize_state();
+    run(state);
+
+    console_log!("Success!");
+}
+
+fn initialize_state() -> State {
     let canvas = Canvas::initialize("canvas");
     let sprite_shader = SpriteShader::new(&canvas);
     let pikachu_texture = Texture::new(&canvas, "pikachu");
-    let mut pikachu_sprite = Sprite::new(&canvas, &sprite_shader, &pikachu_texture);
 
-    canvas.clear();
-    pikachu_sprite.set_position(100f32, 250f32);
-    pikachu_sprite.draw();
+    let mut pikachus = Vec::new();
+    let mut pikachu = Sprite::new(&canvas, &sprite_shader, &pikachu_texture);
+    pikachu.set_position(100f32, 250f32);
+//    pikachus.push(pikachu);
 
-    console_log!("Success!");
+    State {
+        canvas,
+        pikachus,
+    }
+}
+
+fn run(state: State) {
+    let tick_ref = Rc::new(RefCell::new(None));
+    let tick_ref_clone = tick_ref.clone();
+
+    *tick_ref_clone.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        console_log!("Tick!");
+        state.canvas.clear();
+        request_animation_frame(tick_ref.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+    request_animation_frame(tick_ref_clone.borrow().as_ref().unwrap());
+}
+
+fn request_animation_frame(func: &Closure<dyn FnMut()>) {
+    let window = web_sys::window().unwrap();
+    window.request_animation_frame(func.as_ref().unchecked_ref()).unwrap();
 }
